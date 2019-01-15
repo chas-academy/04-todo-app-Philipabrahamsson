a1b9f1ff9b104ff7c3537004cdeaca26e9df9b62
Philip Abrahamsson <Philip@Philis-Air.local>
2019-01-15 09:43:27 +0100

    Update completed

--- src/Models/TodoItem.php
+++ src/Models/TodoItem.php
@@ -19,11 +19,21 @@ class TodoItem extends Model
          // Create a new todo
      }
 
-    // // public static function updateTodo($todoId, $title, $completed = null)
-    // // {
-    // //     // TODO: Implement me!
-    // //     // Update a specific todo
-    // // }
+    public static function updateTodo($todoId, $title, $completed = null)
+        {
+            $query = "UPDATE todos
+                        SET title = '$title', created = CURRENT_TIMESTAMP, completed = '$completed'
+                        WHERE id = '$todoId'";
+            
+
+            self::$db->query($query);
+            $result = self::$db->execute();
+            
+            return $result;
+            
+            // TODO: Implement me!
+             // Update a specific todo
+         }
 
     public static function deleteTodo($todoId)
     {
